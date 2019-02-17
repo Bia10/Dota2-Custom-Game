76e441574ff6b247a9c1b501887de0136bfe89ba
maijz128 <maijz128@gmail.com>
2019-02-17 12:18:35 +0800

    实现：重生墓碑

---
+++ GameMode/respawn_tombstone/scripts/vscripts/addon_game_mode.lua
@@ -0,0 +1,26 @@
+
+
+function CAddonTemplateGameMode:InitGameMode()
+
+	-- 不能自动重生
+	-- GameRules:SetHeroRespawnEnabled( false )
+
+	
+	--监听单位死亡
+	ListenToGameEvent( "entity_killed", Dynamic_Wrap( CAddonTemplateGameMode, 'OnEntityKilled' ), self )
+	
+end
+
+
+function CAddonTemplateGameMode:OnEntityKilled( event )
+	local killedUnit = EntIndexToHScript( event.entindex_killed )
+	if killedUnit and killedUnit:IsRealHero() then
+		local newItem = CreateItem( "item_tombstone", killedUnit, killedUnit )
+		newItem:SetPurchaseTime( 0 )
+		newItem:SetPurchaser( killedUnit )
+		local tombstone = SpawnEntityFromTableSynchronous( "dota_item_tombstone_drop", {} )
+		tombstone:SetContainedItem( newItem )
+		tombstone:SetAngles( 0, RandomFloat( 0, 360 ), 0 )
+		FindClearSpaceForUnit( tombstone, killedUnit:GetAbsOrigin(), true )	
+	end
+end
