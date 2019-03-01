890afb17733cfb7a9a0e3459c13b69dca7edad70
maijz128 <maijz128@gmail.com>
2019-03-02 00:27:17 +0800

    更新物品：莱恩的大根 文件结构更新为组件模式 击杀普通单位达到条件后，会累积伤害层数

--- Item/mjz_dagon/dist/scripts/vscripts/items/item_mjz_dagon.lua
+++ Item/mjz_dagon/dist/scripts/vscripts/items/item_mjz_dagon.lua
@@ -1,6 +1,35 @@
 
-
 function OnSpellStart(keys)
+    if IsServer() then
+        _OnSpellStart(keys)
+    end
+end
+
+function OnEquip( keys )
+    if IsServer() then
+        local ability = keys.ability
+        ability._modifier_tooltip = keys.ModifierTooltip 
+        _UpdateTooltip(ability)
+    end
+end
+
+function OnUnequip( keys )
+    if IsServer() then
+        local caster = keys.caster
+        local ability = keys.ability
+        local modifier_tooltip = keys.ModifierTooltip
+
+        caster:RemoveModifierByName(modifier_tooltip)
+    end
+end
+
+function TargetOnDeath(keys)
+    if IsServer() then
+        _TargetOnDeath(keys)
+    end
+end
+
+function _OnSpellStart(keys)
     local caster = keys.caster
     local target = keys.target
     local ability = keys.ability
@@ -23,7 +52,8 @@ function OnSpellStart(keys)
     end
 	
 
-    local kill_count = ability:GetCurrentCharges()
+    -- local kill_count = ability:GetCurrentCharges()
+    local kill_count = ability._kill_count or 0
     local damage = keys.Damage + damage_per_kill * kill_count
 
     --Control Point 2 in Dagon's particle effect takes a number between 400 and 800, depending on its level.    
@@ -31,12 +61,15 @@ function OnSpellStart(keys)
     caster:EmitSound("DOTA_Item.Dagon.Activate")
     
     for j,unit in ipairs(units) do
-        local victim = unit --target
+        local victim = unit
         --添加死亡监听器
-        if victim:IsHero() then
-            local modifier_death = keys.ModifierDeath
-            ability:ApplyDataDrivenModifier(caster, victim, modifier_death, {duration=kill_grace_duration})
+        if not victim:IsIllusion() then
+            if victim:IsRealHero() or victim:IsCreature() then
+                local modifier_death = keys.ModifierDeath
+                ability:ApplyDataDrivenModifier(caster, victim, modifier_death, {duration=kill_grace_duration})
+            end
         end
+       
 
         local dagon_particle = ParticleManager:CreateParticle("particles/items_fx/dagon.vpcf",  PATTACH_ABSORIGIN_FOLLOW, caster)
         ParticleManager:SetParticleControlEnt(dagon_particle, 1, victim, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), false)
@@ -49,33 +82,81 @@ function OnSpellStart(keys)
 end
 
 
-function TargetOnDeath(keys)	
-	--Search for an Urn of Shadows in the aura creator's inventory.  If there are multiple Urns in the player's inventory,
-	--the one with the least charges receives a charge (this may work differently in the standard Dota 2 mode).
-    local urn_with_least_charges = nil
+function _TargetOnDeath(keys)	
+    local victim = keys.unit
+    if victim:IsIllusion() then return end
+
+	local item_dagon = nil
     local ability_item_name = keys.ItemName
-	
+    -- find item
 	for i=0, 5, 1 do
 		local current_item = keys.caster:GetItemInSlot(i)
 		if current_item ~= nil then
 			local current_item_name = current_item:GetName()
 			
 			if current_item_name == ability_item_name then
-				if urn_with_least_charges == nil then
-					urn_with_least_charges = current_item
-				elseif current_item:GetCurrentCharges() < urn_with_least_charges:GetCurrentCharges() then
-					urn_with_least_charges = current_item
+				if item_dagon == nil then
+					item_dagon = current_item
+				elseif current_item:GetCurrentCharges() < item_dagon:GetCurrentCharges() then
+					item_dagon = current_item
 				end
 			end
 		end
-	end
+    end
 
-	if urn_with_least_charges ~= nil then
-		if urn_with_least_charges:GetCurrentCharges() == 0 then
-			urn_with_least_charges:SetCurrentCharges(1)
-		else
-			urn_with_least_charges:SetCurrentCharges(urn_with_least_charges:GetCurrentCharges() + 1)
-		end
+    if item_dagon ~= nil then
+        local can_charge = false
+
+        if victim:IsRealHero() then
+            can_charge = true
+
+        elseif victim:IsCreature() then
+            local ability_level = item_dagon:GetLevel()
+            local creature_enabled = item_dagon:GetLevelSpecialValueFor("creature_enabled", ability_level)
+            local creature_health = item_dagon:GetLevelSpecialValueFor("creature_health", ability_level)
+        
+            if creature_enabled and creature_enabled > 0 then
+                local victim_health = victim:GetMaxHealth()
+                if victim_health > creature_health then
+                    can_charge = true
+                else
+                    local creature_total = item_dagon._creature_total or 0
+                    if (creature_total + victim_health) > creature_health then
+                        can_charge = true
+                        item_dagon._creature_total = 0
+                    else
+                        can_charge = false
+                        item_dagon._creature_total = creature_total + victim_health
+                    end
+                end
+            end
+        end
+
+        if can_charge then
+            --[[
+            if item_dagon:GetCurrentCharges() == 0 then
+                item_dagon:SetCurrentCharges(1)
+            else
+                item_dagon:SetCurrentCharges(item_dagon:GetCurrentCharges() + 1)
+            end
+            ]]
+
+            local kill_count = item_dagon._kill_count or 0
+            item_dagon._kill_count = kill_count + 1
+            
+            _UpdateTooltip(item_dagon)
+        end
 	end
 end
 
+function _UpdateTooltip( item_dagon )
+    local owner = item_dagon:GetOwner()
+    local modifier = item_dagon._modifier_tooltip
+    local count = item_dagon._kill_count or 0
+	-- local newCount = owner:GetModifierStackCount(modifier, owner) + 1
+
+    if not owner:HasModifier(modifier) then
+        item_dagon:ApplyDataDrivenModifier(owner, owner, modifier, nil)
+    end
+    owner:SetModifierStackCount(modifier, owner, count)    
+end
